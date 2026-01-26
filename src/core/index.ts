9391de839aca0f047daac182a2f6a08459b29330
everton <vieiraeverton33@hotmail.com>
2026-01-25 22:30:18 -0300

    feat(core): export full core surface from core/index

---
+++ src/core/index.ts
@@ -0,0 +1,17 @@
+export * from "./scope.js";
+export * from "./signal.js";
+
+export * from "./watch.js";
+export * from "./when.js";
+export * from "./match.js";
+
+export * from "./for.js";
+
+export * from "./dev.js";
+
+export * from "./key.js";
+export * from "./resource.js";
+export * from "./query.js";
+export * from "./mutation.js";
+
+export { batch, measure, mutate } from "./scheduler.js";
